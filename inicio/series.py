3c9224a51b476beab06517e545f6b066fb80c9dc
Yair Andrey Rodriguez Navarro <yarodriguezn@ufpso.edu.co>
2024-05-17 19:31:21 -0500

    Series

---
+++ inicio/series.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+#Series : es una matriz con 2 columnas y tantas filas
+naranjas = pd.Series([4,9,2,6,10,200])
+print(naranjas)
+nombres = pd.Series(["Yair", "Jorge"])
+print(nombres)
